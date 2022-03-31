82d90d20341c0a763277b047e8b592e32e1156f6
Priyesh <priyesh.raj9@gmail.com>
2022-04-01 00:42:58 +0530

    register form error msg + template

--- src/app/components/register-user/register-user.component.ts
+++ src/app/components/register-user/register-user.component.ts
@@ -1,29 +1,67 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import {
+  AbstractControl,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-register-user',
   templateUrl: './register-user.component.html',
-  styleUrls: ['./register-user.component.scss']
+  styleUrls: ['./register-user.component.scss'],
 })
 export class RegisterUserComponent implements OnInit {
   registerForm: FormGroup = new FormGroup({
-    email: new FormControl({}, Validators.required),
-    password: new FormControl({}, Validators.required),
-    confirmPassword: new FormControl({}, Validators.required)
-  })
-  constructor() { }
+    email: new FormControl('', [Validators.required, Validators.email]),
+    password: new FormControl('', [
+      Validators.required,
+      Validators.minLength(6),
+      Validators.pattern(
+        '^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[$@$!%?&])[A-Za-zd$@$!%?&]'
+      ),
+    ]),
+    confirmPassword: new FormControl('', Validators.required),
+  });
+  constructor() {}
 
-  ngOnInit(): void {
-  }
+  ngOnInit(): void {}
 
   getErrorMessage(field: string) {
+    console.log(this.registerForm.get(field)?.errors);
     switch (field) {
       case 'email':
-        return this.registerForm.get(field)?.hasError('required') ? 'Enter an email':
-          this.registerForm.get(field)?.hasError('email') ? 'Enter a valid email' : '' 
+        return this.registerForm.get(field)?.hasError('required')
+          ? 'Enter an email'
+          : this.registerForm.get(field)?.hasError('email')
+          ? 'Enter a valid email'
+          : '';
+      case 'password':
+        return this.registerForm.get(field)?.hasError('required')
+          ? 'Enter a password'
+          : this.registerForm.get(field)?.hasError('length') ||
+            this.registerForm.get(field)?.hasError('pattern')
+          ? `Password must be of: \n
+          Minimum eight characters
+          At least one uppercase letter
+          At least one lowercase letter
+          At least one number
+          At least one special character
+          `
+          : 'Enter a valid password';
+      case 'confirmPassword':
+        return this.registerForm.get(field)?.hasError('required')
+          ? 'Re-enter password'
+          : this.registerForm.get(field)?.hasError('length') ||
+            this.registerForm.get(field)?.hasError('pattern')
+          ? 'Enter password doesnt match'
+          : '';
       default:
-        return                          ;
-    } 
+        return;
+    }
   }
+
+  onSubmit() {}
+
+  onCloseIconClick() {}
 }
